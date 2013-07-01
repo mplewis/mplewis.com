24378b58c81ca71b2f008ba1b85131eed23a1f74
Matthew Lewis <Matthew Lewis>
2013-06-30 22:52:54 -0700

    reset everything

--- js/parse-json-feed.js
+++
@@ -1,8 +0,0 @@
-function parseFeed(jsonData) {
-    var firstPost = jsonData.responseData.feed.entries[0];
-    var postTitle = firstPost.title;
-    var postTime = firstPost.publishedDate;
-    var postLink = firstPost.link;
-    var postTimeAgo = jQuery.timeago(Date.parse(postTime));
-    $('.blog-data').html('<a href="' + postLink + '">' + postTitle + '</a> <em>(' + postTimeAgo + ')</em>');
-}
